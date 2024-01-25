78718f786ef3867b61ca71b2545800408032f283
Alan <sz9751210@gmail.com>
2024-01-25 09:23:47 +0800

    add lifecycle hooks example

---
+++ v-lifecycle/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Vue 生命週期鉤子示例</h1>
+    <button @click="toggleComponent">切換組件顯示/隱藏</button>
+    <LifecycleExample v-if="showComponent" />
+    <!-- 使用v-if 動態控制組件的掛載與卸載 -->
+  </div>
+</template>
+
+<script>
+import LifecycleExample from "./components/LifecycleExample.vue";
+
+export default {
+  name: "App",
+  components: {
+    LifecycleExample,
+  },
+  data() {
+    return {
+      showComponent: true,
+    };
+  },
+  methods: {
+    toggleComponent() {
+      this.showComponent = !this.showComponent;
+    },
+  },
+};
+</script>
+
+<style>
+/* 您可以在這裡添加一些全局樣式 */
+#app {
+  text-align: center;
+}
+</style>
---
+++ v-lifecycle/LifecycleExample.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LifecycleExample',
+    data() {
+      return {
+        message: '初次渲染的訊息',
+      }
+    },
+    // mounted 鉤子：當組件掛載到 DOM 上後執行。
+    mounted() {
+      console.log('組件被掛載');
+      // 假設我們想要在組件掛載 2 秒後改變訊息。
+      setTimeout(() => {
+        this.message = '2秒後更新的訊息';
+      }, 2000);
+    },
+    // updated 鉤子：當組件的響應式數據發生變化且重新渲染後執行。
+    updated() {
+      console.log('組件被更新');
+    },
+    // unmounted 鉤子：當組件即將從 DOM 上卸載時執行。
+    unmounted() {
+      console.log('組件即將被卸載');
+    },
+  }
+  </script>
+  
+  <style>
+  h1 {
+    color: blue;
+  }
+  </style>
+  
