bc0ca78dcb57a866a82af49cc54c3a642f533038
Alan <sz9751210@gmail.com>
2024-01-27 21:41:34 +0800

    calculator init

---
+++ proj/calculator/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <MyCalculator/>
+  </div>
+</template>
+
+<script>
+import MyCalculator from './components/MyCalculator.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyCalculator
+  }
+};
+</script>
+
+<style>
+/* 全局樣式 */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* 其他全局樣式 */
+</style>
---
+++ proj/calculator/src/components/MyCalculator.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mycalculator">
+    <div class="display">{{ currentInput }}</div>
+    <button
+      v-for="button in buttons"
+      :key="button"
+      @click="onButtonClick(button)"
+    >
+      {{ button }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentInput: "",
+      buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "*", "/", "="],
+    };
+  },
+  methods: {
+    onButtonClick(button) {
+      if (button === "=") {
+        this.calculateResult();
+      } else {
+        this.currentInput += button;
+      }
+    },
+    calculateResult() {
+      try {
+        this.currentInput = eval(this.currentInput);
+      } catch (e) {
+        this.currentInput = "Error";
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
